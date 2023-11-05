6639eb595dce213aac5cea0353f16ab93eb57ddb
RicardoMascarenhas <77035625+RicardoMascarenhas@users.noreply.github.com>
2023-11-05 20:48:39 +0000

    Sphere working

--- src/main/java/com/example/demo/objects/Sphere.java
+++ src/main/java/com/example/demo/objects/Sphere.java
@@ -26,45 +26,37 @@ public class Sphere extends Object3D {
 
     @Override
     public boolean intersect(Ray ray, Hit hit) {
-        // The vector from the sphere's center to the ray's origin
         Vector3 originToCenter = ray.getOrigin().subtract(this.position);
 
-        // Calculate the components needed for the quadratic formula
         double a = ray.getDirection().dot(ray.getDirection());
         double b = 2.0 * originToCenter.dot(ray.getDirection());
         double radius = 1;
         double c = originToCenter.dot(originToCenter) - (radius * radius);
 
-        // Calculate the discriminant
         double discriminant = b * b - 4 * a * c;
 
-        // If discriminant is negative, no real roots and the ray does not intersect the sphere
         if (discriminant < 0) {
             return false;
         }
 
-        // Calculate the two points of intersection t0 and t1 (quadratic formula)
         double sqrtDiscriminant = Math.sqrt(discriminant);
         double t0 = (-b - sqrtDiscriminant) / (2 * a);
         double t1 = (-b + sqrtDiscriminant) / (2 * a);
 
-        // We want the closest positive intersection
         double t = (t0 < t1 && t0 >= 0) ? t0 : t1;
 
-        // If t is negative, the sphere is behind the ray
         if (t < 0) {
             return false;
         }
 
-        // Calculate the point of intersection using the ray's equation
         Vector3 P = ray.getOrigin().add(ray.getDirection().multiply(t));
 
-        // Update hit record with the intersection data
+
         hit.setT(t);
-        hit.setTmin(t); // Note: This should probably be compared with the current hit record tmin
+        hit.setTmin(t);
         hit.setPoint(P);
         hit.setMaterial(this.material);
-        hit.setNormal(P.subtract(this.position).normalize()); // Normal should be normalized
+        hit.setNormal(P.subtract(this.position).normalize());
         hit.setFound(true);
 
         return true;
